c7cf13a544571872dff6e856181b5191fe4c9a57
Mackinlay Kevin <kemack83@gmail.com>
2024-06-04 23:53:16 -0300

    Register design completed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Add from '../img/add-image.png';
 
 const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">World Chat</span>
+        <span className="logo">Chat Cloud</span>
         <span className="title">Register</span>
         <form>
           <input type="text" placeholder="display name"></input>
           <input type="email" placeholder="email"></input>
           <input type="password" placeholder="password"></input>
-          <input type="file" />
+          <input style={{display:"none"}} type="file" id='file' />
+          <label htmlFor='file'>
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
 
           <button>Sign up</button>
         </form>
