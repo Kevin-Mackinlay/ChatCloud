02aa952a76c3da55da5b9d2b48fce6de09d28d40
Mackinlay Kevin <kemack83@gmail.com>
2024-06-09 14:20:23 -0300

    working on Chat Component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chats">
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
+        <div className="userChatInfo">
+          <span>Sandra</span>
+          <p>Hey! How are you?</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
+        <div className="userChatInfo">
+          <span>Sandra</span>
+          <p>Hey! How are you?</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
+        <div className="userChatInfo">
+          <span>Sandra</span>
+          <p>Hey! How are you?</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
+        <div className="userChatInfo">
+          <span>Sandra</span>
+          <p>Hey! How are you?</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = () => {
   return (
     <div className='search'>
       <div className="searchFForm">
-        <input type='text'/>
+        <input type='text' placeholder='Find a user'/>
       </div>
       <div className="userChat">
         <img src='https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=400' alt=''/>
