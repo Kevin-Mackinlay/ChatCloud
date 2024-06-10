bf0430a32aa5227a89cdff4db3c9046ffe3ab717
Mackinlay Kevin <kemack83@gmail.com>
2024-06-10 19:43:26 -0300

    working on messages

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Add from '../img/add.png';
+import Attach from '../img/attach.png';
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type='text' placeholder='Type a message...' />
+      <div className='send'>
+        <img src={Attach} alt='' />
+        <input type='file' id='file' style={{ display: 'none' }} />
+        <label htmlFor='file'>
+          <img src={Add} alt='' />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
 
