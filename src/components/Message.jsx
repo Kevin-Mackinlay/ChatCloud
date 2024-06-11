cedcc45f7e5c0e66d72f7610bb3cb852186afa5a
Mackinlay Kevin <kemack83@gmail.com>
2024-06-11 17:12:28 -0300

    working on Firebase

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hey! How are you?</p>
+        <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Message
