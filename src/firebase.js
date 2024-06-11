e9c04814d4f0d573936e3c0bc975bbcae1d634e3
Mackinlay Kevin <kemack83@gmail.com>
2024-06-11 17:17:19 -0300

    working on Firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCY0nb6Dbaaa0cE3EsI6Pqy-r8KtP6NC5Q',
+  authDomain: 'chatcloud-94b94.firebaseapp.com',
+  projectId: 'chatcloud-94b94',
+  storageBucket: 'chatcloud-94b94.appspot.com',
+  messagingSenderId: '144925298252',
+  appId: '1:144925298252:web:4eeacd938c920c17bdeee9',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
